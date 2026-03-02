af3e885e109c83d766937ce513cf3773d1e251c6
Daniel <danielfloresperez15@gmail.com>
2026-03-02 10:08:43 +0100

    Ultima Subida

--- src/main/java/com/daw/cinemadaw/controller/HomeController.java
+++ src/main/java/com/daw/cinemadaw/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.daw.cinemadaw.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.daw.cinemadaw.domain.cinema.Cinema;
 import com.daw.cinemadaw.repository.CinemaRepository;
@@ -29,6 +31,28 @@ public class HomeController {
         List<Cinema> cines = cinemaRepository.findAll();
         model.addAttribute("Lista",cines);
         return "cinemes";
+    }
+
+    @GetMapping("/cinema/{id}")
+    public String detall(@PathVariable Long id, Model model){
+
+        Optional<Cinema> optional = cinemaRepository.findById(id);
+        if(optional.isPresent()){
+            Cinema cinema = optional.get();
+            model.addAttribute("cinema", cinema);
+            return "detail-cinema";
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/cinema/{id}")
+    public String delete(@PathVariable Long id, Model model){
+
+        Optional<Cinema> optional = cinemaRepository.findById(id);
+        if(optional.isPresent()){
+            Cinema cinema = optional.get();
+        }
     }
 
 }

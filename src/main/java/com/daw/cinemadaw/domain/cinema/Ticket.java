41260eeb3b8d1c2ae8146e80557db62ceb68adbb
Daniel <danielfloresperez15@gmail.com>
2026-04-20 09:31:03 +0200

    first commit

--- src/main/java/com/daw/cinemadaw/domain/cinema/Ticket.java
+++ src/main/java/com/daw/cinemadaw/domain/cinema/Ticket.java
@@ -6,8 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
+@Table(name = "tickets", uniqueConstraints = @UniqueConstraint(columnNames = {"screening_id", "seat_id"}))
 public class Ticket {
 
     @Id
@@ -26,7 +29,8 @@ public class Ticket {
     @ManyToOne
     private Seat seat;
 
-    public Ticket() {}
+    public Ticket() {
+    }
 
     public Ticket(double price, Order order, Screening screening, Seat seat) {
         this.price = price;
@@ -35,18 +39,43 @@ public class Ticket {
         this.seat = seat;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public Order getOrder() { return order; }
-    public void setOrder(Order order) { this.order = order; }
+    public double getPrice() {
+        return price;
+    }
 
-    public Screening getScreening() { return screening; }
-    public void setScreening(Screening screening) { this.screening = screening; }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-    public Seat getSeat() { return seat; }
-    public void setSeat(Seat seat) { this.seat = seat; }
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Screening getScreening() {
+        return screening;
+    }
+
+    public void setScreening(Screening screening) {
+        this.screening = screening;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
 }
